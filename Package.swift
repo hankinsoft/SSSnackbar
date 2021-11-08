413fc8e17dcf04a49ddb2ad9be795984cdb95fe7
Kyle Hankinson <kyle@hankinsoft.com>
2021-11-08 15:05:50 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -11,6 +11,10 @@ let package = Package(
             name: "SSSnackbar",
             targets: ["SSSnackbar"]
         ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         .target(
