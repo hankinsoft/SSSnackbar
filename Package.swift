05de423906316e0962115d941213781bd1c98997
Kyle Hankinson <kyle@hankinsoft.com>
2021-11-08 14:27:55 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+	// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "SSSnackbar",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "SSSnackbar",
+            targets: ["SSSnackbar"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SSSnackbar",
+            dependencies: [],
+            path: "Pod/Classes",
+        )
+    ]
+)
