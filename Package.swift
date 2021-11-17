16918e57b7f1ab5ca9245700a19d41ec6be94d5a
Kyle Hankinson <kyle@hankinsoft.com>
2021-11-17 11:49:54 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,27 +1,24 @@
 // swift-tools-version:5.3
-import PackageDescription
+ import PackageDescription
 
-let package = Package(
-    name: "SSSnackbar",
-    platforms: [
-        .iOS(.v13),
-    ],
-    products: [
-        .library(
-            name: "SSSnackbar",
-            targets: ["SSSnackbar"]
-        ),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "SSSnackbar",
-            dependencies: [],
-            path: "Pod/Classes",
-            publicHeadersPath: "Pod/Classes"
-        )
-    ]
-)
+  let package = Package(
+     name: "SSSnackbar",
+     platforms: [
+         .iOS(.v13),
+     ],
+     products: [
+         .library(
+             name: "SSSnackbar",
+             targets: ["SSSnackbar"]
+         ),
+     ],
+     dependencies: [],
+     targets: [
+         .target(
+             name: "SSSnackbar",
+             dependencies: [],
+             path: "Pod",
+             publicHeadersPath: "Classes"
+         )
+     ]
+ ) 
